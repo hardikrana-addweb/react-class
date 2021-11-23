9ae92029226c1db5878082defecdad3ee2466d6d
hardikrana-addweb <hardik@addwebsolution.in>
2021-11-23 18:30:24 +0530

    added input

--- src/pages/ClassDemo.js
+++ src/pages/ClassDemo.js
@@ -7,15 +7,20 @@ class ClassDemo extends React.Component {
     this.state = {
       showChild: false,
       counter: 0,
+      inputData:''
     };
     this.increment = this.increment.bind(this);
     this.showButton = this.showButton.bind(this);
+    this.onTextChanges = this.onTextChanges.bind(this)
   }
   increment() {
     this.setState({ counter: this.state.counter + 1 });
   }
   showButton() {
     this.setState({ showChild: !this.state.showChild });
+  }
+  onTextChanges(e){
+    this.setState({inputData: e.target.value})
   }
   render() {
     return (
@@ -29,6 +34,8 @@ class ClassDemo extends React.Component {
         {this.state.showChild && (
           <button onClick={this.increment}>{this.state.counter}</button>
         )}
+        <input type='text' onChange={this.onTextChanges} />
+        {this.state.inputData!=='' && this.state.inputData}
       </>
     );
   }
